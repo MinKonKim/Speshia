a4e2f9718ecbc18cd5638a537a7aab5c00420839
minkonkim <alsrhs98@gmail.com>
2025-06-03 18:12:41 +0900

    chore:  supabase 연동

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { createClient } from './supabase'
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
+
+export const supabase = await createClient()
