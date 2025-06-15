429f4ac1eb73f7f47758900c31fb7458842f38a0
alsrhs98 <alsrhs98@gmail.com>
2025-06-15 18:35:06 +0900

    chore : 글자 스타일 맞춤.

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
       <MainHeader />
       <div className="bg-primary bg-primary-500 h-2.5 w-full">
         {/* TODO: 공통 스타일링 추가 */}
-        <h1 className="text-">페이지 타이틀</h1>
+        <h1 className="title">페이지 타이틀</h1>
         <h2 className="text-headline">섹션 제목</h2>
         <h3 className="text-title">모달 제목</h3>
         <p className="text-body">이건 일반 본문 텍스트입니다.</p>
