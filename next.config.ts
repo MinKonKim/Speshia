21b914ad5f78cf6301d5b612c78513f734762833
alsrhs98 <alsrhs98@gmail.com>
2025-06-15 18:28:48 +0900

    chore : next.config.ts 수정정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true, // ✅ 정확한 키
-  } as any satisfies NextConfig,
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
